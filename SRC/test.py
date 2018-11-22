cffd214a84cd7c3a987141b3589d31134248d8a0
Khimer <45174146+Khimer@users.noreply.github.com>
2018-11-22 12:57:05 +0300

    Update test.py

--- SRC/test.py
+++ SRC/test.py
@@ -8,11 +8,11 @@ model=load_model(sys.argv[1])
 while(True):
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
-    cv2.imwrite(sys.argv[6] + str(i) + '.jpeg', frame)
+    cv2.imwrite(sys.argv[2] + str(i) + '.jpeg', frame)
     testgen = ImageDataGenerator(rescale=1. / 255)
-    now_generator = testgen.flow_from_directory(sys.argv[6], target_size=(150, 150), batch_size=1,
+    now_generator = testgen.flow_from_directory(sys.argv[3], target_size=(150, 150), batch_size=1,
                                                 class_mode='binary')
-    a=1-(model.predict_generator(now_generator, 1//1))
+    a=int(1-(model.predict_generator(now_generator, 1//1)))
     print(a)
     if cv2.waitKey(1) & 0xFF == ord('q'):
 	        break
