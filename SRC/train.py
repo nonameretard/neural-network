f8a99d2bb2db2f67bfdf7f761ac76723a453199b
Khimer <45174146+Khimer@users.noreply.github.com>
2018-11-22 11:31:39 +0300

    Update train.py

--- SRC/train.py
+++ SRC/train.py
@@ -1,2 +1,22 @@
 import sys
+from keras.preprocessing.image import ImageDataGenerator
 model.load(sys.argv[1])
+import cv2
+i=1
+now_dir=sys.argv[6]
+img_width, img_height=150,150
+cap = cv2.VideoCapture(0)
+a=model.predict_generator(test_generator, nb_test_samples//batch_size)
+while(True):
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    cv2.imwrite(sys.argv[6] + str(i) + '.jpeg', frame)
+    testgen = ImageDataGenerator(rescale=1. / 255)
+    now_generator = testgen.flow_from_directory(now_dir, target_size=(img_width, img_height), batch_size=1,
+                                                class_mode='binary')
+    a=1-(model.predict_generator(now_generator, 1//1))
+    print(a)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+	        break
+cap.release()
+cv2.destroyAllWindows()
